42581d400eea8663c3b91e79bab53679d4081ccf
gavincraig <gcraig197@gmail.com>
2024-01-29 15:31:15 +0000

    feat: (Examples) - Scoped context to TryIt

--- packages/elements-core/src/components/TryIt/TryItWithRequestSamples.tsx
+++ packages/elements-core/src/components/TryIt/TryItWithRequestSamples.tsx
@@ -2,6 +2,7 @@ import { Box, InvertTheme, VStack } from '@stoplight/mosaic';
 import { Request as HarRequest } from 'har-format';
 import * as React from 'react';
 
+import ExamplesContext from '../../context/ExamplesContext';
 import { RequestSamples } from '../RequestSamples';
 import { ResponseExamples, ResponseExamplesProps } from '../ResponseExamples/ResponseExamples';
 import { TryIt, TryItProps } from './TryIt';
@@ -11,19 +12,39 @@ export type TryItWithRequestSamplesProps = Omit<TryItProps, 'onRequestChange'> &
 
 export const TryItWithRequestSamples: React.FC<TryItWithRequestSamplesProps> = ({ hideTryIt, ...props }) => {
   const [requestData, setRequestData] = React.useState<HarRequest | undefined>();
+
+  const [globalSelectedExample, setGlobalSelectedExample] = React.useState('Approved Auth MIT Subsequent Stored');
+
   return (
-    <VStack spacing={6}>
-      {!hideTryIt && (
-        <InvertTheme>
-          <Box>
-            <TryIt {...props} onRequestChange={setRequestData} />
-          </Box>
-        </InvertTheme>
-      )}
+    <ExamplesContext.Provider value={{ globalSelectedExample, setGlobalSelectedExample }}>
+      <VStack spacing={6}>
+        <ul>
+          <li>
+            <button onClick={() => setGlobalSelectedExample('Approved Auth Basic')}>Approved Auth Basic</button>
+          </li>
+          <li>
+            <button onClick={() => setGlobalSelectedExample('Approved Auth CIT Onetime Stored')}>
+              Approved Auth CIT Onetime Stored
+            </button>
+          </li>
+          <li>
+            <button onClick={() => setGlobalSelectedExample('Approved Auth MIT Subsequent Stored')}>
+              Approved Auth MIT Subsequent Stored
+            </button>
+          </li>
+        </ul>
+        {!hideTryIt && (
+          <InvertTheme>
+            <Box>
+              <TryIt {...props} onRequestChange={setRequestData} />
+            </Box>
+          </InvertTheme>
+        )}
 
-      {requestData && <RequestSamples request={requestData} />}
+        {requestData && <RequestSamples request={requestData} />}
 
-      <ResponseExamples {...props} />
-    </VStack>
+        <ResponseExamples {...props} />
+      </VStack>
+    </ExamplesContext.Provider>
   );
 };
