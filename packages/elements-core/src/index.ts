116d0f165b14becc0324a1a895c21d6b76c5e3b7
AhalyaPai <ahalyapai08@gmail.com>
2025-01-06 12:03:47 +0000

    feat: resolve type warnings

--- packages/elements-core/src/index.ts
+++ packages/elements-core/src/index.ts
@@ -1,20 +1,20 @@
-export { Docs, DocsProps, ExtensionAddonRenderer, ExtensionRowProps, ParsedDocs } from './components/Docs';
+export { Docs, ParsedDocs } from './components/Docs';
+export type { DocsProps, ExtensionAddonRenderer, ExtensionRowProps } from './components/Docs';
 export { DeprecatedBadge } from './components/Docs/HttpOperation/Badges';
-export { ExportButton, ExportButtonProps } from './components/Docs/HttpService/ExportButton';
+export { ExportButton } from './components/Docs/HttpService/ExportButton';
+export type { ExportButtonProps } from './components/Docs/HttpService/ExportButton';
 export { ResponsiveSidebarLayout } from './components/Layout/ResponsiveSidebarLayout';
 export { SidebarLayout } from './components/Layout/SidebarLayout';
 export { LinkHeading } from './components/LinkHeading';
 export { Logo } from './components/Logo';
 export { DefaultSMDComponents } from './components/MarkdownViewer/CustomComponents/CodeComponent';
-export {
-  CustomComponentMapping,
-  MarkdownComponentsProvider,
-} from './components/MarkdownViewer/CustomComponents/Provider';
+export { MarkdownComponentsProvider } from './components/MarkdownViewer/CustomComponents/Provider';
+export type { CustomComponentMapping } from './components/MarkdownViewer/CustomComponents/Provider';
 export { ReactRouterMarkdownLink } from './components/MarkdownViewer/CustomComponents/ReactRouterLink';
 export { NonIdealState } from './components/NonIdealState';
 export { PoweredByLink } from './components/PoweredByLink';
 export { TableOfContents } from './components/TableOfContents';
-export {
+export type {
   CustomLinkComponent,
   TableOfContentsGroup,
   TableOfContentsItem,
@@ -22,7 +22,8 @@ export {
   TableOfContentsNodeGroup,
 } from './components/TableOfContents/types';
 export { findFirstNode } from './components/TableOfContents/utils';
-export { TryIt, TryItProps, TryItWithRequestSamples, TryItWithRequestSamplesProps } from './components/TryIt';
+export { TryIt, TryItWithRequestSamples } from './components/TryIt';
+export type { TryItProps, TryItWithRequestSamplesProps } from './components/TryIt';
 export { HttpMethodColors, NodeTypeColors, NodeTypeIconDefs, NodeTypePrettyName } from './constants';
 export { MockingProvider } from './containers/MockingProvider';
 export { InlineRefResolverProvider } from './context/InlineRefResolver';
@@ -38,9 +39,9 @@ export { useParsedValue } from './hooks/useParsedValue';
 export { useResponsiveLayout } from './hooks/useResponsiveLayout';
 export { useRouter } from './hooks/useRouter';
 export { Styled, withStyles } from './styled';
-export { Divider, Group, ITableOfContentsTree, Item, ParsedNode, RoutingProps, TableOfContentItem } from './types';
+export type { Divider, Group, ITableOfContentsTree, Item, ParsedNode, RoutingProps, TableOfContentItem } from './types';
 export { isHttpOperation, isHttpService, isHttpWebhookOperation } from './utils/guards';
-export { ReferenceResolver } from './utils/ref-resolving/ReferenceResolver';
+export type { ReferenceResolver } from './utils/ref-resolving/ReferenceResolver';
 export { createResolvedObject } from './utils/ref-resolving/resolvedObject';
 export { slugify } from './utils/string';
 export { createElementClass } from './web-components/createElementClass';
